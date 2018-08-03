f3b13e4aea21d83daf78fb1f95e77de5abdf70f2
Tyler Buchea <tyler@buchea.com>
2018-08-03 16:03:12 -0700

    Add pull down drawer

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,44 @@
 import React from 'react';
 import styled from 'styled-components';
 import Icon from 'react-native-vector-icons/dist/FontAwesome';
+import { LayoutAnimation } from 'react-native';
 
 const NavbarContainer = styled.View`
   height: 80px;
-  background-color: #e9e9ef;
+`;
+
+const FilterContainer = styled.View`
+  height: 200px;
+  width: 100%;
   padding-top: 20px;
+  padding-bottom: 5px;
+  background-color: #e9e9ef;
+  background-color: #4fb797;
+  position: relative;
+  top: -120px;
   align-items: center;
-  justify-content: center;
+  justify-content: flex-end;
 `;
 
 export default class Navbar extends React.PureComponent {
+  state = { isFilterOpen: false };
+
+  toggleFilter = () => {
+    LayoutAnimation.configureNext(LayoutAnimation.Presets.easeInEaseOut);
+    this.setState({ isFilterOpen: !this.state.isFilterOpen });
+  };
+
   render() {
+    const { isFilterOpen } = this.state;
     return (
       <NavbarContainer testID="Navbar">
-        <Icon name="chevron-down" size={18} />
+        <FilterContainer style={isFilterOpen && { top: 0 }}>
+          <Icon
+            name={isFilterOpen ? 'chevron-up' : 'chevron-down'}
+            size={18}
+            onPress={this.toggleFilter}
+          />
+        </FilterContainer>
       </NavbarContainer>
     );
   }

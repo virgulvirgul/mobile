0e4447e22acb8c19194e374967f9f3f693b2d6df
Vladimir Kokovic <vlada.kokovic@gmail.com>
2018-08-24 13:22:55 +0200

    Link Android and iOS to App Center

---
+++ ios/Pods/Target Support Files/Pods-please/Pods-please-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_please : NSObject
+@end
+@implementation PodsDummy_Pods_please
+@end
---
+++ ios/Pods/Target Support Files/Pods-pleaseTests/Pods-pleaseTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_pleaseTests : NSObject
+@end
+@implementation PodsDummy_Pods_pleaseTests
+@end

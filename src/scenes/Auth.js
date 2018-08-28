6174e808612ce5d5f6cb9472bcecf83dc231e4c0
Vladimir Kokovic <vlada.kokovic@gmail.com>
2018-08-28 14:09:07 +0200

    Auth0, log in with credentials

--- src/scenes/Auth.js
+++ src/scenes/Auth.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import styled from 'styled-components';
 import { Button, Text, TextInput } from 'react-native';
+import Auth0 from 'react-native-auth0';
 
 const AuthContainer = styled.View`
   width: 100%;
@@ -12,21 +13,44 @@ const AuthContainer = styled.View`
   padding-right: 20px;
 `;
 
+const auth0 = new Auth0({ domain: 'staging-please.eu.auth0.com', clientId: 'hdP9sSzartSH2lvR2oABAWd0Cq7UUQVv' });
+
 export default class Auth extends React.PureComponent {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+      password: ""
+    };
+  };
+
+  logInWithCredentials = () => {
+    auth0
+      .auth
+      .passwordRealm({username: this.state.username, password: this.state.password, realm: "Username-Password-Authentication"})
+      .then(console.log)
+      .catch(console.error);
+  };
+
   render() {
     return (
       <AuthContainer testID="Auth">
         <TextInput
           placeholder="E-mail"
           style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+          onChangeText={(value) => this.setState({username: value})}
+          value={this.state.username}
         />
         <TextInput
           secureTextEntry={true}
           placeholder="Password"
           style={{ height: 40, borderColor: 'gray', borderWidth: 1, marginTop: 20 }}
+          onChangeText={(value) => this.setState({password: value})}
+          value={this.state.password}
         />
-        <Button title="Log in" />
+        <Button title="Log in" onPress={this.logInWithCredentials}/>
       </AuthContainer>
     );
-  }
+  };
 }

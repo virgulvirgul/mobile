e043a262e63ae31aa4f2c5c0aa241e4f8f5f4469
dm0lz <ducrouxolivier@gmail.com>
2018-09-01 17:34:19 +0200

    properly renaming Container

--- src/components/Container.js
+++ src/components/Container.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const View = styled.View`
+const Container = styled.View`
   flex: 1;
 `;
 
-export default View;
+export default Container;

6e3039cf621efa48ad5b845abd1157e2bd2e971c
fcmatteo <facumatteo@gmail.com>
2018-09-05 09:14:05 -0300

    Move capitalizeFirstChar to utils

--- src/libs/utils.js
+++ src/libs/utils.js
@@ -4,6 +4,9 @@ function getLang() {
   return config.DEFAULT_LOCALE;
 }
 
+const capitalizeFirstChar = string => string.charAt(0).toUpperCase() + string.slice(1);
+
 export {
   getLang,
+  capitalizeFirstChar,
 };
